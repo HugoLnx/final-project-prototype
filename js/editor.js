50a81f45578b6a9847eabb70a7d0b7779700a56a
Hugo Roque <hugolnx@gmail.com>
2016-11-08 18:16:54 -0200

    Style to the left side of events window

--- js/editor.js
+++ js/editor.js
@@ -2,6 +2,7 @@ var ELEMENTS = {
   map: $('#map'),
   mapImage: $('#map img')
 };
+var eventTemplate = Handlebars.compile($("#event-template").html());
 
 var dataMap = JSON.parse(localStorage['$dataMap']);
 var events = [];
@@ -20,12 +21,35 @@ var eventDialog = {
   ev: null,
   create: function() {
     this.element = $("<div class='event-dialog'></div>");
-    $(document.body).append(this.element);
-    this.element.dialog({autoOpen: false, modal: true, resizable: false});
+    this.element.dialog({
+      autoOpen: false,
+      modal: true,
+      resizable: false,
+      width: 900,
+      height: 600
+    });
   },
   openFor: function(ev) {
     this.ev = ev;
-    this.element.html(ev.data.name);
+    var firstpage = ev.data.pages[0];
+    var image = firstpage.image;
+    this.element.html(eventTemplate({
+      image: {
+        path: "/img/characters/" + image.characterName + ".png",
+        x: (image.characterIndex % 4)*144 + image.pattern*48,
+        y: Math.floor(image.characterIndex / 4)*192 + (image.direction/2-1)*48
+      }
+             /*
+"image": {
+            "tileId": 0,
+            "characterName": "People3",
+            "direction": 2,
+            "pattern": 1,
+            "characterIndex": 7
+          },
+              */
+    }));
+    this.element.dialog("option", "title", "Event ID: " + ev.data.id);
     this.element.dialog("open");
   },
   apply: function() {
