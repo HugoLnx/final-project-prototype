c4e02b8d30abe213e9e6cd5713f0206a770b58fb
Hugo Roque <hugolnx@gmail.com>
2016-11-26 18:32:22 -0200

    Tolerance to null events

--- js/map.js
+++ js/map.js
@@ -3,18 +3,23 @@ var events = [];
 
 for(var i = 1; i<dataMap.events.length; i++) {
   var ev = dataMap.events[i];
-  var div = $("<div class='map-event'></div>");
-  div.data("id", ev.id);
-  div.css({left: ev.x * 48 + 4, top: ev.y * 48 + 4});
-  ELEMENTS.map.append(div);
-  events[i] = {data: ev, div: div};
+  if(ev) {
+    var div = $("<div class='map-event'></div>");
+    div.data("id", ev.id);
+    div.css({left: ev.x * 48 + 4, top: ev.y * 48 + 4});
+    ELEMENTS.map.append(div);
+    events[i] = {data: ev, div: div};
+  }
 }
 
-ELEMENTS.map.on("click", ".map-event", function(ev) {
+ELEMENTS.map.on("click", ".map-event", function(event) {
   var dataMap = JSON.parse(localStorage['$dataMap']);
   for(var i = 1; i<dataMap.events.length; i++) {
-    events[i].data = dataMap.events[i];
+    var ev = dataMap.events[i];
+    if(ev) {
+      events[i].data = ev;
+    }
   }
-  var eventId = $(ev.target).data("id");
+  var eventId = $(event.target).data("id");
   EventDialog.openFor(events[eventId]);
 });
